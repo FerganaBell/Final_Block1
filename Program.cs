57dd1dcada4f3a44ba51d132e8514bd0cbe77664
FerganaBell <tina_vlad@mai.ru>
2024-11-17 22:06:27 +0300

    Подсчет количества строк <=3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿ 
+﻿ void Main()
+{ 
+// Вводим исходный массив строк с клавиатуры 
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string input = Console.ReadLine();
+        string[] InitialArray = input.Split(' ');
+
+  // Считаем количество строк, длина которых меньше или равна 3 символам
+        int count = 0;
+        for (int i = 0; i < InitialArray.Length; i++)
+        {
+            if (InitialArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+       
+}
+
+Main();
